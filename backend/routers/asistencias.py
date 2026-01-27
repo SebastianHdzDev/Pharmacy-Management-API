c77049b9c5d4406be6be80c6f86408f848d633f2
SebastianHdzDev <sebastian.hernandez72@outlook.com>
2026-01-27 01:21:32 -0600

    feat(asistencias): agregar endpoints CRUD para asistencias

---
+++ backend/routers/asistencias.py
@@ -0,0 +1,61 @@
+from fastapi import APIRouter, HTTPException, Path, Depends
+from sqlmodel import Session, select
+from typing import Annotated, List
+
+from backend.db import get_session
+from backend.models import Usuario, Asistencia
+from backend.schemas import * 
+
+router = APIRouter()
+
+@router.post("/asistencias", response_model=Asistencia)
+async def registrar_asistencia(
+    info_asistencia: AsistenciaCreate,
+    session: Session=Depends(get_session)
+) -> Asistencia:
+    usuario = session.get(Usuario, info_asistencia.idUsuario)
+    if not usuario: 
+        raise HTTPException(status_code=404, detail="NO SE ENCONTRO EL USUARIO CON EL ID INDICADO")
+    asistencia=Asistencia.model_validate(info_asistencia)
+    session.add(asistencia)
+    session.commit()
+    session.refresh(asistencia)
+    return asistencia
+
+
+@router.get("/asistencias", response_model=List[Asistencia])
+async def obtener_asistencias(session: Session=Depends(get_session))->List[Asistencia]:
+    statement = select(Asistencia)
+    resultados = session.exec(statement)
+    asistencias = resultados.all()
+    return asistencias
+
+
+@router.patch("/asistencias/{asistencia_id}", response_model=AsistenciaRead)
+async def actualizar_asistencia(
+    asistencia_id : Annotated[int, Path(title="ID de la asistencia")],
+    asistencia_info : AsistenciaUpdate,
+    session: Session = Depends(get_session)
+)-> AsistenciaRead:
+    asistencia = session.get(Asistencia, asistencia_id)
+    if not asistencia:
+        raise HTTPException(status_code=404, detail="ASISTENCIA NO ENCONTRADA")
+    datos = asistencia_info.model_dump(exclude_unset=True)
+    asistencia.sqlmodel_update(datos)
+
+    session.add(asistencia)
+    session.commit()
+    session.refresh(asistencia)
+    return asistencia
+
+
+@router.delete("/asistencias/{asistencia_id}")
+async def eliminar_asistencia(
+    asistencia_id : Annotated[int, Path(title="ID de la asistencia")],
+    session : Session = Depends(get_session)
+):
+    asistencia = session.get(Asistencia, asistencia_id)
+    if not asistencia:
+        raise HTTPException(status_code=404, detail="ASISTENCIA NO ENCONTRADA")
+    session.delete(asistencia)
+    session.commit()
