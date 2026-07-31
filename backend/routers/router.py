94c38b6207001dca67694ec9b958faabf219512c
SebastianHdzDev <sebastian.hernandez72@outlook.com>
2026-07-30 23:34:27 -0600

    refactor(core): delete empty router detalles-venta

--- backend/routers/router.py
+++ backend/routers/router.py
@@ -1,17 +1,16 @@
-from asistencias import router as asistencias_router
-from compras import router as compras_router
-from detalles_venta import router as detalles_venta_router
-from facturas import router as facturas_router
+from .asistencias import router as asistencias_router
+from .compras import router as compras_router
+from .facturas import router as facturas_router
 from fastapi import APIRouter
-from inventarios import router as inventarios_router
-from medicamentos import router as medicamentos_router
-from proveedores import router as proveedores_router
-from sucursales import router as sucursales_router
-from tickets import router as tickets_router
-from usuarios import router as usuarios_router
-from ventas import router as ventas_router
+from .inventarios import router as inventarios_router
+from .medicamentos import router as medicamentos_router
+from .proveedores import router as proveedores_router
+from .sucursales import router as sucursales_router
+from .tickets import router as tickets_router
+from .usuarios import router as usuarios_router
+from .ventas import router as ventas_router
 
-from auth import router as auth_router
+from .auth import router as auth_router
 
 router = APIRouter()
 
@@ -30,8 +29,6 @@ router.include_router(proveedores_router, prefix="/proveedores", tags=["Proveedo
 router.include_router(compras_router, prefix="/compras", tags=["Compras"])
 
 router.include_router(inventarios_router, prefix="/inventarios", tags=["Inventarios"])
-
-router.include_router(detalles_venta_router, prefix="/detalles-venta", tags=["Detalles de Venta"])
 
 router.include_router(tickets_router, prefix="/tickets", tags=["Tickets"])
 
