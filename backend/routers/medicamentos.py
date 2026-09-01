c0b00409e1392f45b79cba3a55caf30d77ea0a51
SebastianHdzDev <sebastian.hernandez72@outlook.com>
2026-09-01 00:55:04 -0600

    feat(api): apply pagination with offset and unified search of medicines

--- backend/routers/medicamentos.py
+++ backend/routers/medicamentos.py
@@ -1,12 +1,14 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, Path
+from sqlalchemy import func
 from sqlmodel import Session, select
 
 from backend.auth import get_current_active_user, verify_admin
 from backend.db import get_session
+from backend.dependencies import paginacion_comun
 from backend.models import Medicamento, Usuario
-from backend.schemas import MedicamentoCreate, MedicamentoRead, MedicamentoUpdate
+from backend.schemas import MedicamentoCreate, MedicamentoRead, MedicamentoUpdate, PaginacionMedicamentos
 
 router = APIRouter(dependencies=[Depends(get_current_active_user)])
 
@@ -23,14 +25,50 @@ async def crear_medicamento(
     return medicamento
 
 
-@router.get("", response_model=list[MedicamentoRead])
+@router.get("/", response_model=PaginacionMedicamentos)
 async def obtener_medicamentos(
+    nombre: str | None,
+    dosis: str | None,
+    viaAdministracion: str | None,
+    laboratorio: str | None,
+    tipoMedicamento: str | None,
+    usoTerapeutico: str | None,
+    requiereReceta: bool | None,
+    paginacion: dict = Depends(paginacion_comun),
     current_user: Usuario = Depends(get_current_active_user),
     session: Session = Depends(get_session)
-) -> list[MedicamentoRead]:
+) -> PaginacionMedicamentos:
     statement = select(Medicamento).where(Medicamento.estaActivo)
-    resultados = session.exec(statement)
-    return resultados.all()
+
+    if nombre:
+        statement = statement.where(Medicamento.nombre.ilike(f"%{nombre}%"))
+    
+    if dosis:
+        statement = statement.where(Medicamento.dosis.ilike(f"%{dosis}%"))
+    
+    if viaAdministracion:
+        statement = statement.where(Medicamento.viaAdministracion.ilike(f"%{viaAdministracion}%"))
+
+    if laboratorio:
+        statement = statement.where(Medicamento.laboratorio.ilike(f"%{laboratorio}%"))
+
+    if tipoMedicamento:
+        statement = statement.where(Medicamento.tipoMedicamento.ilike(f"%{tipoMedicamento}%"))
+
+    if usoTerapeutico:
+        statement = statement.where(Medicamento.usoTerapeutico.ilike(f"%{usoTerapeutico}%"))
+
+    if requiereReceta is not None:
+        statement = statement.where(Medicamento.requiereReceta == requiereReceta)
+
+    count_statement = statement.with_only_columns(func.count())
+    total_records = session.exec(count_statement).one()
+
+    # ejecuta la consulta con todos los where indicados y dependiendo de parametros
+    statement = statement.offset(paginacion["skip"]).limit(paginacion["limit"])
+    resultados = session.exec(statement).all()
+    
+    return {"total": total_records, "items": resultados}
 
 
 @router.get("/{medicamento_id}", response_model=MedicamentoRead)
@@ -79,42 +117,4 @@ async def eliminar_medicamento(
     session.commit()
 
 
-@router.get("/", response_model=list[MedicamentoRead])
-async def buscar_medicamentos(
-    nombre: str | None,
-    dosis: str | None,
-    viaAdministracion: str | None,
-    laboratorio: str | None,
-    tipoMedicamento: str | None,
-    usoTerapeutico: str | None,
-    requiereReceta: bool | None,
-    current_user: Usuario = Depends(get_current_active_user),
-    session: Session = Depends(get_session)
-) -> list[MedicamentoRead]:
-    statement = select(Medicamento).where(Medicamento.estaActivo)
 
-    if nombre:
-        statement = statement.where(Medicamento.nombre.ilike(f"%{nombre}%"))
-    
-    if dosis:
-        statement = statement.where(Medicamento.dosis.ilike(f"%{dosis}%"))
-    
-    if viaAdministracion:
-        statement = statement.where(Medicamento.viaAdministracion.ilike(f"%{viaAdministracion}%"))
-
-    if laboratorio:
-        statement = statement.where(Medicamento.laboratorio.ilike(f"%{laboratorio}%"))
-
-    if tipoMedicamento:
-        statement = statement.where(Medicamento.tipoMedicamento.ilike(f"%{tipoMedicamento}%"))
-
-    if usoTerapeutico:
-        statement = statement.where(Medicamento.usoTerapeutico.ilike(f"%{usoTerapeutico}%"))
-
-    if requiereReceta is not None:
-        statement = statement.where(Medicamento.requiereReceta == requiereReceta)
-
-    # ejecuta la consulta con todos los where indicados y dependiendo de parametros
-    resultados = session.exec(statement)
-    
-    return resultados.all()
