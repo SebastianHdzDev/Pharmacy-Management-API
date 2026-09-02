c9f6f32086c23ecf4961b9fa219d0a27eb9df794
SebastianHdzDev <sebastian.hernandez72@outlook.com>
2026-09-02 01:04:28 -0600

    feat(api): initialize context variable after token validation

--- backend/auth.py
+++ backend/auth.py
@@ -11,6 +11,7 @@ from pydantic import BaseModel
 from sqlmodel import Session, select
 
 from backend.models import RolEnum, TokenBloqueado, Usuario
+from backend.context import current_user_id
 
 from .db import get_session
 
@@ -95,6 +96,7 @@ def get_current_user(token:str = Depends(oauth2_scheme), db: Session = Depends(g
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Usuario no existe",
                 headers={"WWW-Authenticate":"Bearer"})
+    current_user_id.set(user.idUsuario)
     return user
 
 
